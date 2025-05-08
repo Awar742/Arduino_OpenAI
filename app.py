4574d0c92073990af677ac6cdf2f96353bda981a
Awar742 <71485768+Awar742@users.noreply.github.com>
2025-05-08 10:35:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from bottle import route, run
+from bottle import route, run, request, template
+
+latest_data = {"temperature": None, "humidity": None}
 
 @route('/')
 def home():
-    return "Hello, Bottle!"
+    return template("""
+        <h1>Дані з Arduino</h1>
+        <p>Температура: {{temp}} °C</p>
+        <p>Вологість: {{hum}} %</p>
+    """, temp=latest_data["temperature"], hum=latest_data["humidity"])
+
+@route('/update', method='POST')
+def update():
+    try:
+        data = request.json
+        latest_data["temperature"] = data.get("temperature")
+        latest_data["humidity"] = data.get("humidity")
+        return {"status": "ok"}
+    except:
+        return {"status": "error", "message": "Невірний формат"}
 
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=8080)
+    run(host='0.0.0.0', port=8080, debug=True)
